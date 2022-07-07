3d07698d81824c817ff6c89ff51dd163b405f781
inasbek <b_ines@live.com>
2022-07-07 10:10:18 +0200

    Initial Commit

---
+++ twitter_clone/twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_cloneApp.swift
+//  twitter_clone
+//
+//  Created by Inas Bekkouche on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
